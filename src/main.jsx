057a2a443b7a47e4c1acff377bd1cda1c440bd90
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-25 13:06:11 +0100

    refactor content app working

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
